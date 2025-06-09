89b0ff1b73b388b52ef5d7c890607c054465c759
learners-100 <kunnylasya05@gmail.com>
2025-06-09 10:03:30 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 15167
+let accountEmail = "lasya@gmail.com"
+var accPassword = "12347"
+accountCity="Hyderabad"
+// accountId= 2 not allowed
+console.log(accountId);
+accPassword = "123999"
+accountEmail= "sreyas@gmail.com"
+accountCity="Chennai"
+
+console.log(accountCity);
+
